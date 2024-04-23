1f9105b4786c4262efa02c9928a3fe11fd08f9ac
Emmanuel Inua <artofnuel@gmail.com>
2024-04-23 12:04:11 +0100

    Update BlogMeta.js

--- app/blog/components/BlogMeta.js
+++ app/blog/components/BlogMeta.js
@@ -1,8 +1,85 @@
-import { getBlogSlug } from "@/app/api/blogSlug";
+import { client } from "@/sanity/lib/client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const BlogMeta = async () => {
-  const data = await getBlogSlug();
-  return <></>;
-};
+export async function generateMetadata({ params }) {
+  const [blog, setBlog] = useState(null);
+  const { slug } = useParams;
 
-export default BlogMeta;
+  useEffect(() => {
+    const getBlogData = async () => {
+      const query = `*[_type == 'blog' && slug.current == $slug]{
+            _createdAt,
+            author,
+            title,
+            category,
+            description,
+            date,
+            'slug': slug.current,
+            'authorImage': image.asset->url,
+            'image': image.asset->url,
+            'alt': image.alt,
+          }`;
+
+      try {
+        const result = await client.fetch(query, { slug });
+        if (result && result.length > 0) {
+          const blog = result[0];
+
+          return {
+            title: blog.title,
+            description: blog.description,
+            openGraph: {
+              title: blog.title,
+              description: blog.description,
+              url: `http://www.hhlfoundation.org/${blog.slug}`,
+              siteName: "Hassan and Hussein Lecky Foundation",
+              type: "article",
+              images: [
+                {
+                  url: blog.image,
+                  alt: blog.alt,
+                },
+              ],
+            },
+            twitter: {
+              card: "summary_large_image",
+              title: blog.title,
+              description: blog.description,
+              creator: "@hhlfoundation",
+              image: {
+                url: blog.image,
+                alt: blog.alt,
+              },
+            },
+          };
+        }
+      } catch (error) {
+        console.error("Error fetching blog data:", error);
+      }
+      return {
+        title: "Hassan and Hussein Lecky Foundation",
+        description: "Empowering the youth",
+        openGraph: {
+          title: "Hassan and Hussein Lecky Foundation",
+          description: "Empowering the youth",
+          url: "http://www.hhlfoundation.org",
+          siteName: "Hassan and Hussein Lecky Foundation",
+          type: "website",
+        },
+        twitter: {
+          card: "summary_large_image",
+          title: "Hassan and Hussein Lecky Foundation",
+          description: "Empowering the youth",
+          creator: "@hhlfoundation",
+          image: {
+            url: "/path/to/default/image.jpg",
+            alt: "Default Image Alt",
+          },
+        },
+      };
+    };
+
+    getBlogData();
+  }, [slug]);
+}
